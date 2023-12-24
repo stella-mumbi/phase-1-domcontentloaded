93c0f012adec0ba28bf9e993d03f75f42b27ce13
stella-mumbi <stella.mumbi@student.moringaschool.com>
2023-12-24 12:41:24 +0300

    completed assignmnet

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+
 // Your code goes here
+// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+
+    const textParagraph = document.getElementById("text");
+    textParagraph.textContent = "This is really cool!";
+  });
+
